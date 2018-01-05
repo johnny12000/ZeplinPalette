45bfe19239b910c45a0e01bd4a08840c56a6844e
johnny12000 <ristic.nikola@icloud.com>
2018-01-05 15:58:23 +0100

    Project skeleton added

---
+++ ZeplinPalette/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ZeplinPalette
+//
+//  Created by Nikola Ristic on 1/5/18.
+//  Copyright © 2018 fr. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
